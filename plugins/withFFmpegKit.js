beaedd1369d13be486aa1cf8c51ade41ae09e260
Runteryaa <runteryaa@github.com>
2026-07-31 12:55:24 +0300

    fix: use local config plugin for ffmpegKitPackage min variant

---
+++ plugins/withFFmpegKit.js
@@ -0,0 +1,20 @@
+const { withProjectBuildGradle } = require('@expo/config-plugins');
+
+module.exports = function withFFmpegKit(config) {
+  return withProjectBuildGradle(config, (config) => {
+    if (config.modResults.language === 'groovy') {
+      const extBlock = `
+        ext {
+            ffmpegKitPackage = "min"
+        }
+      `;
+      if (!config.modResults.contents.includes('ffmpegKitPackage')) {
+        config.modResults.contents = config.modResults.contents.replace(
+          /buildscript\s*{/,
+          `buildscript {\n${extBlock}`
+        );
+      }
+    }
+    return config;
+  });
+};
